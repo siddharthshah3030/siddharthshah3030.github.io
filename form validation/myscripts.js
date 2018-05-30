a7631cc16b67f1353be8b72052e3fe430fc4bf6b
siddharthshah3030 <siddharthshah3030@gmail.com>
2018-05-30 16:49:52 +0530

    name fiel working

--- form validation/myscripts.js
+++ form validation/myscripts.js
@@ -1,9 +1,19 @@
 function validateForm() {
     var x = document.forms["form"]["name"].value;
-    if (x == "") {
-        alert("Name must be filled out");
-        return false;
-    }
+    var hasNumber = /\d/;
+  if(  hasNumber.test(x) )
+
+      {
+        alert('Please input alphabet characters only');
+      return true;
+      }
+      else
+      {
+        alert('Your name have accepted : you can try another');
+      return false;
+      }
 }
 
 console.log("sid here");
+
+ 
