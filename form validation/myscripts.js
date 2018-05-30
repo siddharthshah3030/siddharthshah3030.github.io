949bc830e4a9783472ff80a436c9911f2bf22737
siddharthshah3030 <siddharthshah3030@gmail.com>
2018-05-30 20:11:36 +0530

    working form

--- form validation/myscripts.js
+++ form validation/myscripts.js
@@ -1,92 +1,88 @@
-function name() {
-
-    var x = document.forms["form"]["name"].value;
-    var hasNumber = /\d/;
-  if(  hasNumber.test(x) )
-
-      {
-        alert('Please input alphabet characters only');
-      return true;
-      }
-      else
-      {
-        alert('Your name have accepted : you can try another');
-      return false;
-      }
-}
 
 console.log("sid here");
 
+var myInput = document.forms["form"]["password"];
+var low = document.getElementById("low");
+var cap = document.getElementById("cap");
+var num = document.getElementById("num");
+var len = document.getElementById("len");
 
-function age() {
-  a = document.getElementById("age").value;
-    var age = parseInt(a, 10);
-  if(  age<21 )
-
-      {
-        alert('age must be greater then 20');
-      return true;
-      }
-      else
-      {
-        alert('age is fine');
-      return false;
-      }
-}
-function pa() {
-  var myInput = document.forms["form"]["password"];
+myInput.onkeyup = function() {
 
   //valiadte lowwer case letters
   var lowerCaseLetters = /[a-z]/g;
 if(myInput.value.match(lowerCaseLetters)) {
+  low.classList.remove("no");
+  low.classList.add("yes");
 } else {
-  console.log("invalid");
+  low.classList.remove("yes");
+  low.classList.add("no");
 }
 
 // Validate capital letters
 var upperCaseLetters = /[A-Z]/g;
 if(myInput.value.match(upperCaseLetters)) {
+  cap.classList.remove("no");
+
+  cap.classList.add("yes");
+
 } else {
   console.log("invalid");
+  cap.classList.remove("yes");
+
+  cap.classList.add("no");
 
 }
 
 // Validate numbers
 var numbers = /[0-9]/g;
 if(myInput.value.match(numbers)) {
+  num.classList.remove("no");
+
+  num.classList.add("yes");
+
 } else {
-  console.log("invalid");
+  num.classList.remove("yes");
+
+  num.classList.add("no");
+
 
 }
 
 // Validate length
 if(myInput.value.length >= 8) {
+  len.classList.remove("no");
+
+  len.classList.add("yes");
+
 } else {
-  console.log("invalid");
+  len.classList.remove("yes");
+
+  len.classList.add("no");
+
 
 }
 }
 
-function pass() {
+var pass = document.forms["form"]["pass"];
+pass.onkeyup = function() {
   a = document.getElementById("pass1").value;
   b = document.getElementById("pass2").value
   if( a===b )
-
-      {
-        alert('password');
-      return true;
+      {    match.classList.remove("no");
+          match.classList.add("yes");
       }
       else
-      {
-        alert('not same password');
-      return false;
+      {      match.classList.remove("yes");
+            match.classList.add("no");
       }
 }
-function hide() {
-    var element = document.getElementById("age");
-    element.classList.add("vis");
-}
+// function hide() {
+//     var element = document.getElementById("age");
+//     element.classList.add("vis");
+// }
 
+// name
 
 var namec = document.getElementById("name");
 var names = document.getElementById("names");
@@ -98,20 +94,31 @@ if(  hasNumber.test(x) )
     {
       names.classList.remove("yes");
       names.classList.add("no");
-    return true;
     }
     else
     {
       names.classList.remove("no");
       names.classList.add("yes");
-    return false;
     }
 
 }
-hide();
-function v(){
-// let t =  age();
-//    t = name();
-   t = pass();
-   t = pa();
+
+var age = document.getElementById("age");
+var ages = document.getElementById("ages");
+
+age.onkeyup = function() {
+  var a = document.forms["form"]["age"].value;
+  var age = parseInt(a, 10);
+if(  age<21 )    {
+      ages.classList.remove("yes");
+      ages.classList.add("no");
+    return true;
+    }
+    else
+    {
+      ages.classList.remove("no");
+      ages.classList.add("yes");
+    return false;
+    }
+
 }
