cf6018d97b7e8ba04fb8e24649394077d4cd59e2
siddharthshah3030 <siddharthshah3030@gmail.com>
2018-06-14 00:56:23 +0530

    mobile navbar

--- wall.js/demo/navbar.js
+++ wall.js/demo/navbar.js
@@ -1,14 +1,36 @@
 var navbar = document.getElementById("navy");
 var x =  document.getElementById("croxx");
 function mymenu() {
-  navbar.classList.toggle("notnow");
+  console.log(x.className);
+  console.log(navbar.style.display);
+  if (x.className === "dog") {
+    x.className = "change";
+} else {
+    x.className = "dog";
+}
+if(  navbar.style.display == "flex"){
+  navbar.style.display = "none";
 
-    x.classList.toggle("change");
+} else {
+  navbar.style.display = "flex";
+
+}
+
+
 
 }
 function menu() {
-  navbar.classList.toggle("notnow");
+  if(  navbar.style.display == "none"){
+    navbar.style.display = "flex";
 
+  } else {
+    navbar.style.display = "none";
 
+  }
+  if (x.className === "dog") {
+    x.className = "change";
+} else {
+    x.className = "dog";
+}
 }
 // document.getElementByClass("nav-link").addEventListener("click", menu());
