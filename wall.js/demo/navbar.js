28aaa7f227b71dd659a446db2a6be8f493202b83
siddharthshah3030 <siddharthshah3030@gmail.com>
2018-06-14 01:04:37 +0530

    mobile navbar

--- wall.js/demo/navbar.js
+++ wall.js/demo/navbar.js
@@ -1,36 +1,40 @@
 var navbar = document.getElementById("navy");
 var x =  document.getElementById("croxx");
-function mymenu() {
-  console.log(x.className);
-  console.log(navbar.style.display);
-  if (x.className === "dog") {
-    x.className = "change";
-} else {
-    x.className = "dog";
-}
-if(  navbar.style.display == "flex"){
-  navbar.style.display = "none";
+// function mymenu() {
+//   console.log(x.className);
+//   console.log(navbar.style.display);
+//   if (x.className === "dog") {
+//     x.className = "change";
+// } else {
+//     x.className = "dog";
+// }
+// if(  navbar.style.display == "flex"){
+//   navbar.style.display = "none";
+//
+// } else {
+//   navbar.style.display = "flex";
+//
+// }
 
-} else {
-  navbar.style.display = "flex";
-
-}
-
+var mysize  = window.innerWidth
+|| document.documentElement.clientWidth
+|| document.body.clientWidth;
 
 
-}
+
 function menu() {
-  if(  navbar.style.display == "none"){
-    navbar.style.display = "flex";
+  if(mysize<650){
+  if(  navbar.style.display == "flex"){
+    navbar.style.display = "none";
 
   } else {
-    navbar.style.display = "none";
+    navbar.style.display = "flex";
 
   }
   if (x.className === "dog") {
     x.className = "change";
 } else {
     x.className = "dog";
-}
+}}
 }
 // document.getElementByClass("nav-link").addEventListener("click", menu());
