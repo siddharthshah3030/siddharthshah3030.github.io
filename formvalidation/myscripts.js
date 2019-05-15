5039447aebd990259ad1a305ffe40d851e7e8d94
SiD <siddharthshah3030@gmail.com>
2019-05-15 20:00:33 +0530

    linting form validation

--- formvalidation/myscripts.js
+++ formvalidation/myscripts.js
@@ -1,5 +1,3 @@
-
-
 var myInput = document.forms["form"]["password"];
 var low = document.getElementById("low");
 var cap = document.getElementById("cap");
@@ -11,91 +9,73 @@ myInput.onkeyup = function() {
 
   //valiadte lowwer case letters
   var lowerCaseLetters = /[a-z]/g;
-if(myInput.value.match(lowerCaseLetters)) {
-low.classList.remove("no");
-  low.classList.add("yes");
-  submit();
-} else {
+  if (myInput.value.match(lowerCaseLetters)) {
+    low.classList.remove("no");
+    low.classList.add("yes");
+    submit();
+  } else {
+    low.classList.remove("yes");
+    low.classList.add("no");
+    submit();
+  }
 
-  low.classList.remove("yes");
-  low.classList.add("no");
-  submit();
-}
+  // Validate capital letters
+  var upperCaseLetters = /[A-Z]/g;
+  if (myInput.value.match(upperCaseLetters)) {
+    cap.classList.remove("no");
 
-// Validate capital letters
-var upperCaseLetters = /[A-Z]/g;
-if(myInput.value.match(upperCaseLetters)) {
-  cap.classList.remove("no");
+    cap.classList.add("yes");
+    submit();
+  } else {
+    cap.classList.remove("yes");
 
-  cap.classList.add("yes");
-  submit();
+    cap.classList.add("no");
+    submit();
+  }
 
-} else {
-  cap.classList.remove("yes");
+  // Validate numbers
+  var numbers = /[0-9]/g;
+  if (myInput.value.match(numbers)) {
+    num.classList.remove("no");
 
-  cap.classList.add("no");
-  submit();
+    num.classList.add("yes");
+    submit();
+  } else {
+    num.classList.remove("yes");
 
-}
+    num.classList.add("no");
+    submit();
+  }
 
-// Validate numbers
-var numbers = /[0-9]/g;
-if(myInput.value.match(numbers)) {
-  num.classList.remove("no");
+  // Validate length
+  if (myInput.value.length >= 8) {
+    len.classList.remove("no");
 
-  num.classList.add("yes");
-  submit();
+    len.classList.add("yes");
+    submit();
+  } else {
+    len.classList.remove("yes");
 
-} else {
-  num.classList.remove("yes");
-
-  num.classList.add("no");
-  submit();
-
-
-}
-
-// Validate length
-if(myInput.value.length >= 8) {
-  len.classList.remove("no");
-
-  len.classList.add("yes");
-  submit();
-
-} else {
-  len.classList.remove("yes");
-
-  len.classList.add("no");
-  submit();
-
-
-}
-}
+    len.classList.add("no");
+    submit();
+  }
+};
 
 var pass = document.forms["form"]["pass"];
 pass.onkeyup = function() {
   var two = this;
   a = document.getElementById("pass1").value;
-  b = document.getElementById("pass2").value
-  if( a===b )
-      {    match.classList.remove("no");
-  match.classList.add("yes");
-  submit();
-      }
-      else
-
-      {
-         match.classList.remove("yes");
-            match.classList.add("no");
-            submit();
-      }
-}
-// function hide() {
-//     var element = document.getElementById("age");
-//     element.classList.add("vis");
-// }
-
-// name
+  b = document.getElementById("pass2").value;
+  if (a === b) {
+    match.classList.remove("no");
+    match.classList.add("yes");
+    submit();
+  } else {
+    match.classList.remove("yes");
+    match.classList.add("no");
+    submit();
+  }
+};
 
 var namec = document.getElementById("name");
 var names = document.getElementById("names");
@@ -104,49 +84,37 @@ namec.onkeyup = function() {
   var three = this;
   var x = document.forms["form"]["name"].value;
   var hasNumber = /\d/;
-if(  hasNumber.test(x) )
+  if (hasNumber.test(x)) {
+    document.getElementById("names").innerHTML = "numbers ain't allowed";
+    names.classList.remove("yes");
+    names.classList.add("no");
 
-{
-  document.getElementById("names").innerHTML = "numbers ain't allowed"
-      names.classList.remove("yes");
-      names.classList.add("no");
-
-    submit();  }
-    else
-
-    {
-      document.getElementById("names").innerHTML = "name is fine"
-      names.classList.remove("no");
-      names.classList.add("yes");
-      submit();
-
-    }
-
-}
+    submit();
+  } else {
+    document.getElementById("names").innerHTML = "name is fine";
+    names.classList.remove("no");
+    names.classList.add("yes");
+    submit();
+  }
+};
 var phonec = document.getElementById("phone");
 var phones = document.getElementById("phones");
 
 phonec.onkeyup = function() {
   var p = document.forms["form"]["phone"].value;
-if(  p.length == 10 )
+  if (p.length == 10) {
+    document.getElementById("phones").innerHTML = "Don't worry we wont spam";
+    phones.classList.remove("no");
+    phones.classList.add("yes");
+    submit();
+  } else {
+    document.getElementById("phones").innerHTML = "it has to be 10 digits";
+    phones.classList.remove("yes");
+    phones.classList.add("no");
 
-{
-
-        document.getElementById("phones").innerHTML = "Don't worry we wont spam"
-        phones.classList.remove("no");
-        phones.classList.add("yes");
-    submit();  }
-    else
-
-    {  document.getElementById("phones").innerHTML = "it has to be 10 digits"
-          phones.classList.remove("yes");
-          phones.classList.add("no");
-
-      submit();
-
-    }
-
-}
+    submit();
+  }
+};
 
 var age = document.getElementById("age");
 var ages = document.getElementById("ages");
@@ -156,87 +124,86 @@ age.onkeyup = function() {
 
   var a = document.forms["form"]["age"].value;
   var age = parseInt(a, 10);
-if(  age<21 )    {
-  document.getElementById("ages").innerHTML = "must be above 20"
-      ages.classList.remove("yes");
+  if (age < 21) {
+    document.getElementById("ages").innerHTML = "must be above 20";
+    ages.classList.remove("yes");
 
-      ages.classList.add("no");
-      submit();
+    ages.classList.add("no");
+    submit();
+  } else if (age > 99) {
+    document.getElementById("ages").innerHTML =
+      "above 99, sorry but you're way more older";
+    ages.classList.remove("yes");
 
-    }
-    else if (  age > 99) {
-      document.getElementById("ages").innerHTML = "above 99, sorry but you're way more older"
-      ages.classList.remove("yes");
+    ages.classList.add("no");
+    submit();
+  } else {
+    document.getElementById("ages").innerHTML =
+      "congrats you're old enough for this site";
+    ages.classList.remove("no");
+    ages.classList.add("yes");
 
-      ages.classList.add("no");
-      submit();
-    }
-    else
-    {
-
-      document.getElementById("ages").innerHTML = "congrats you're old enough for this site"
-      ages.classList.remove("no");
-      ages.classList.add("yes");
-
-      submit();
-    }
-
-}
+    submit();
+  }
+};
 
 var email = document.getElementById("email");
 
 email.onkeyup = function() {
   var five = this;
 
-  var evalue  = document.forms["form"]["email"].value;
+  var evalue = document.forms["form"]["email"].value;
   var emails = document.getElementById("emails");
   var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 
-  if(filter.test(evalue)){
-    document.getElementById("emails").innerHTML = "it looks fine email"
-  emails.classList.remove("no");
-  emails.classList.add("yes");
-  submit();
+  if (filter.test(evalue)) {
+    document.getElementById("emails").innerHTML = "it looks fine email";
+    emails.classList.remove("no");
+    emails.classList.add("yes");
+    submit();
+  } else {
+    document.getElementById("emails").innerHTML =
+      "Don't give a fake mail, please";
+    emails.classList.remove("yes");
+    emails.classList.add("no");
+    submit();
 
-
-} else {
-  document.getElementById("emails").innerHTML = "Don't give a fake mail, please"
-  emails.classList.remove("yes");
-  emails.classList.add("no");
-  submit();
-
-
-  submit();
-
-}
-}
+    submit();
+  }
+};
 var sub = document.getElementById("submitb");
 
- function submit() {
-  var so = document.getElementsByClassName('no');
-if (so.length > 0) {
-  document.getElementById("h4").innerHTML = "form not filled properly"
-  document.getElementById("submitb").value = "Can't Submit Now"
+function submit() {
+  var so = document.getElementsByClassName("no");
+  if (so.length > 0) {
+    document.getElementById("h4").innerHTML = "form not filled properly";
+    document.getElementById("submitb").value = "Can't Submit Now";
 
-  sub.classList.remove("hi")
-  sub.classList.add("disable")
+    sub.classList.remove("hi");
+    sub.classList.add("disable");
 
-  console.log("disable")
-}
-  else if( myInput.value && pass.value && namec.value && age.value && phone.value && email.value) {
-    console.log("hi")
-    document.getElementById("h4").innerHTML = "form filled correctly and congrats"
-    document.getElementById("submitb").value = "you can submit now"
+    console.log("disable");
+  } else if (
+    myInput.value &&
+    pass.value &&
+    namec.value &&
+    age.value &&
+    phone.value &&
+    email.value
+  ) {
+    console.log("hi");
+    document.getElementById("h4").innerHTML =
+      "form filled correctly and congrats";
+    document.getElementById("submitb").value = "you can submit now";
 
-    sub.classList.remove("disable")
-    sub.classList.add("hi")
-  }
-  else {
-    document.getElementById("h4").innerHTML = "kindly fill all the fields"
-    document.getElementById("submitb").value = "Can't Submit Now"
+    sub.classList.remove("disable");
+    sub.classList.add("hi");
+  } else {
+    document.getElementById("h4").innerHTML = "kindly fill all the fields";
+    document.getElementById("submitb").value = "Can't Submit Now";
 
-    sub.classList.remove("hi")
-    sub.classList.add("disable")
-    console.log("disable")
+    sub.classList.remove("hi");
+    sub.classList.add("disable");
+    console.log("disable");
   }
 }
